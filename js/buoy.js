975e7f2de79363091462b5ae2de34d5860368aee
Anders Bälter <anders.balter@npolar.no>
2015-10-06 14:05:16 +0200

    Sorting and deployment data

--- js/buoy.js
+++ js/buoy.js
@@ -1,8 +1,13 @@
+/* jshint undef: true, unused: true */
+/* globals glacier: true */
+'use strict';
+
 var globe,
 	buoyList = [],
 	buoyListElem = document.getElementById('buoyList'),
 	ui,
-	itemInfo;
+	itemInfo,
+	buildItemInfo;
 
 globe = new glacier.GlobeScene('scene', {
 	background:			glacier.color.BLACK,
@@ -16,6 +21,28 @@ globe.run();
 
 // itemInfo setup
 (function () {
+	buildItemInfo = function (dataProps) {
+		var  dt, dd, dl, span;
+
+		if (typeof dataProps !== 'object') {
+			span = document.createElement('span');
+			span.innerHTML = dataProps;
+			return span;
+		}
+		dl = document.createElement('DL');
+		// jshint -W089
+		for(var prop in dataProps) {
+			dt = document.createElement('DT');
+			dt.innerHTML = prop;
+			dl.appendChild(dt);
+
+			dd = document.createElement('DD');
+			dd.appendChild(buildItemInfo(dataProps[prop]));
+			dl.appendChild(dd);
+		}
+		return dl;
+	};
+
 	itemInfo = {
 		callbackUID: null,
 		currentItem: null,
@@ -25,46 +52,25 @@ globe.run();
 		},
 		move: function(latLng) {
 			var screenPos = globe.latLngToScreen(latLng);
-			
+
 			this.element.style.left = (screenPos.x - (this.element.offsetWidth / 2)) + 'px';
 			this.element.style.top = (screenPos.y - (this.element.offsetHeight + 10)) + 'px';
 		},
 		setProperties: function(properties) {
-			var child, dl = this.element.querySelector('DL'), dt, dd;
-			
-			while((child = dl.lastChild)) {
-				dl.removeChild(child);
-			}
-			
-			for(prop in properties) {
-				if (['title', '_id', 'id', '_rev', 'schema', 'links', 'sequence'].indexOf(prop) == -1) {
-					dt = document.createElement('DT');
-					dt.innerHTML = prop;
-					dl.appendChild(dt);
-					
-					dd = document.createElement('DD');
-					dd.innerHTML = properties[prop];
-					dl.appendChild(dd);
+			var dataDiv = this.element.querySelector('#data'), dataProps = {};
+
+			dataDiv.innerHTML = '';
+			for (var prop in properties) {
+				if (['title', '_id', 'id', '_rev', 'schema', 'links', 'sequence'].indexOf(prop) === -1) {
+					dataProps[prop] = properties[prop];
 				}
 			}
-			/*
-			dataItem.properties.links.forEach(function (link) {
-				var dt = document.createElement('dt');
-				var dd = document.createElement('dd');
-				var a = document.createElement('a');
-				dt.innerHTML = link.title;
-				a.href = link.href;
-				a.target = '_blank';
-				a.innerHTML = link.href;
-				dd.appendChild(a);
-				dl.appendChild(dt);
-				dl.appendChild(dd);
-			});
-			*/
+
+			dataDiv.appendChild(buildItemInfo(dataProps));
 		},
 		setTitle: function(title, color) {
 			var elem = this.element.querySelector('H4');
-			
+
 			elem.innerHTML = title;
 			elem.style.color = (color ? color.toHtmlString() : null);
 		},
@@ -72,7 +78,7 @@ globe.run();
 			this.element.classList.remove('hidden');
 		}
 	};
-	
+
 	itemInfo.element.querySelector('.close').addEventListener('click', function() {
 		itemInfo.hide();
 	});
@@ -81,12 +87,12 @@ globe.run();
 ui = {
 	createCheckbox: function(item) {
 		var checkbox = document.createElement('INPUT');
-		
+
 		checkbox.type = 'checkbox';
 		checkbox.name = item.name;
 		checkbox.classList.add('toggle');
 		checkbox.checked = true;
-		
+
 		checkbox.toggle = function(event) {
 			if(event && (event.target === this)) {
 				if(event.target.checked) {
@@ -101,69 +107,69 @@ ui = {
 					item.data.hide();
 				}
 			}
-			
+
 			if(itemInfo.currentItem === item) {
 				if(!this.checked) {
 					itemInfo.hide();
 				}
 			}
 		};
-		
+
 		checkbox.addEventListener('change', checkbox.toggle);
-		
+
 		checkbox.check = function() {
 			this.checked = true;
 			item.data.show();
 		};
-		
+
 		checkbox.uncheck = function() {
 			this.checked = false;
 			item.data.hide();
-		}
-		
+		};
+
 		return checkbox;
 	},
 	titleLink: function (item) {
 		var a = document.createElement('a');
-		
-		a.addEventListener('click', function (e) {
+
+		a.addEventListener('click', function () {
 			ui.focusItem(item);
 		});
-		
+
 		a.style.color = item.color.toString();
 		a.style.cursor = 'pointer';
 		a.innerHTML = item.name;
-		
+
 		return a;
 	},
 	addButton: function () {
 		var button = document.getElementById('toggle');
-		
+
 		button.addEventListener('click', function() {
 			buoyList.forEach(function(buoy) {
 				buoy.checkbox.toggle();
 			});
 		});
-		
+
 		button.classList.remove('hidden');
 	},
 	focusItem: function (dataItem) {
 		itemInfo.hide();
 		dataItem.checkbox.check();
-		
+
 		itemInfo.currentItem = dataItem;
 		itemInfo.setTitle(dataItem.name, dataItem.color);
 		itemInfo.setProperties(dataItem.properties);
-		
+
 		globe.focus(dataItem.position, function(latLng) {
 			if(itemInfo.callbackUID !== null) {
 				globe.removeRunCallback(itemInfo.callbackUID);
 			}
-			
-			itemInfo.callbackUID = globe.addRunCallback(function(dtime) {
+
+			itemInfo.callbackUID = globe.addRunCallback(function() {
 				itemInfo.move(latLng);
 			});
-			
+
 			itemInfo.show();
 		});
 	}
@@ -171,15 +177,15 @@ ui = {
 
 function addBuoy(geojson) {
 	var color = new glacier.Color(geojson.features[0].properties.IMEI % 0xFFFFFF);
-	
+
 	globe.addData(geojson, color, function(uid, data) {
 		var features = data.geoJSON.features,
-			first = features[0],
+			//first = features[0],
 			last = features[features.length - 1],
 			listItem,
 			buoyItem,
 			checkbox;
-		
+
 		buoyList.push({
 			name: last.properties.title,
 			imei: last.properties.IMEI,
@@ -189,7 +195,7 @@ function addBuoy(geojson) {
 			color: color,
 			position: new glacier.Vector2(last.properties.longitude, last.properties.latitude)
 		});
-		
+
 		buoyItem = buoyList[buoyList.length - 1];
 		listItem = document.createElement('LI');
 		checkbox = ui.createCheckbox(buoyItem);
@@ -198,7 +204,7 @@ function addBuoy(geojson) {
 		listItem.appendChild(ui.titleLink(buoyItem));
 		buoyListElem.appendChild(listItem);
 	});
-};
+}
 
 function onAddSuccess() {
 	ui.addButton();
@@ -206,19 +212,21 @@ function onAddSuccess() {
 }
 
 (function () {
-	glacier.load('//api.npolar.no/oceanography/buoy/?q=&facets=IMEI&size-facet=99&format=json&limit=0', function(data) {
-		var imeis = [], buoysAdded = 0;
+	glacier.load('//api.npolar.no/oceanography/buoy/?q=&facets=title&size-facet=99&format=json&limit=0', function(data) {
+		var titles = [], buoysAdded = 0;
 		data = JSON.parse(data);
-		
-		data.feed.facets[0].IMEI.forEach(function(imei) {
-			imeis.push(imei.term);
+
+		data.feed.facets[0].title.forEach(function(title) {
+			titles.push(title.term);
 		});
-		
-		imeis.forEach(function(imei) {
-			glacier.load('//api.npolar.no/oceanography/buoy/?q=&format=geojson&limit=all&sort=measured&filter-IMEI=' + imei, function(data) {
+
+		titles.sort();
+
+		titles.forEach(function(title) {
+			glacier.load('//api.npolar.no/oceanography/buoy/?q=&format=geojson&limit=all&sort=measured&filter-title=' + title, function(data) {
 				addBuoy(JSON.parse(data));
-				
-				if(++buoysAdded >= imeis.length) {
+
+				if(++buoysAdded >= titles.length) {
 					onAddSuccess();
 				}
 			});
